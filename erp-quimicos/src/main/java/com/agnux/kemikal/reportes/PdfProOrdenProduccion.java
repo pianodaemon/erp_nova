f471b151a163d20f8328984c697920210de89b68
fmora <fmora@ce483f98-b20a-4c78-8f99-c15376fc4320>
2012-12-19 18:57:29 +0000

    git-svn-id: http://ironman.agnux.com/repositorios/ErpAgnuxV1@103 ce483f98-b20a-4c78-8f99-c15376fc4320

--- erp-quimicos/src/main/java/com/agnux/kemikal/reportes/PdfProOrdenProduccion.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/reportes/PdfProOrdenProduccion.java
@@ -1251,7 +1251,7 @@ public class PdfProOrdenProduccion {
             celdaAut.setBorderWidthTop(0);
             celdaAut.setBorderWidthRight(0);
             celdaAut.setColspan(5);
-            celdaAut.setRowspan(2);
+            celdaAut.setRowspan(4);
             celdaAut.setBorderWidthLeft(0);
             tablaAut.addCell(celdaAut);
             
