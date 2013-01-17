e4fb392f46b15dda0550b984ffd1812a1b281d68
fmora <fmora@ce483f98-b20a-4c78-8f99-c15376fc4320>
2013-01-17 00:41:42 +0000

    git-svn-id: http://ironman.agnux.com/repositorios/ErpAgnuxV1@236 ce483f98-b20a-4c78-8f99-c15376fc4320

--- erp-quimicos/src/main/java/com/agnux/kemikal/controllers/HomeController.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/controllers/HomeController.java
@@ -7,7 +7,12 @@ package com.agnux.kemikal.controllers;
 import com.agnux.common.obj.UserSessionData;
 import com.agnux.kemikal.interfacedaos.HomeInterfaceDao;
 import com.agnux.common.obj.ResourceProject;
+import com.agnux.kemikal.interfacedaos.GralInterfaceDao;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.logging.Level;
@@ -15,6 +20,9 @@ import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +31,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 
 @Controller
@@ -36,6 +48,15 @@ public class HomeController {
     @Autowired
     @Qualifier("daoHome")
     private HomeInterfaceDao HomeDao;
+    
+    @Autowired
+    @Qualifier("daoGral")
+    private GralInterfaceDao gralDao;
+    
+    public GralInterfaceDao getGralDao() {
+        return gralDao;
+    }
+    
     
     public HomeInterfaceDao getHomeDao() {
         return HomeDao;
@@ -53,6 +74,36 @@ public class HomeController {
         this.resource = resource;
     }
     
+    
+    
+    /*Variables para guardar valores DOF*/
+    String title = "";
+    String description = "";
+    String valueDate = "";
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getValueDate() {
+        return valueDate;
+    }
+
+    public void setValueDate(String valueDate) {
+        this.valueDate = valueDate;
+    }
     
     
     
@@ -132,6 +183,9 @@ public class HomeController {
         x = x.addObject("empresa", userdata.getRazonSocialEmpresa());
         x = x.addObject("sucursal", userdata.getSucursal());
         
+        /*Para actualizar el tipo de cambio*/
+        this.tipoCambioServiceMethod(Integer.parseInt(succes.get("id")));
+        
         //String userId = String.valueOf(userdata.getUserId());
         //System.out.println("id_de_usuario: "+userId);
         
@@ -145,11 +199,83 @@ public class HomeController {
         return x;
     }
     
+    public void tipoCambioServiceMethod(Integer user_id){
+        Date date = new Date();
+        
+        getXmlV2("http://dof.gob.mx/indicadores.xml");
+        
+        //121--Proceso Actualizador Tipo de cambion
+        String data_string = "121___new___"+user_id+"___0___"+this.getDescription()+"___dolar%";
+        String extra_data_array = "'sin datos'";
+        
+        //System.out.println("antes de jdbc data_string:"+data_string+"     extra_data_array:"+extra_data_array);
+        String retorno = this.jobTiposMoneda(data_string, extra_data_array);
+        /*
+        if(retorno.equals("1")){
+            System.out.println("despues de jdbc data_string:"+data_string+"     extra_data_array:"+extra_data_array);
+        }else{
+            System.out.println("despues de jdbc data_string:"+data_string+"     extra_data_array:"+extra_data_array);
+        }
+        */
+    }
+    
+    public void getXmlV2(String urlString){
+        try {
+            
+            URL url = new URL(urlString);
+            URLConnection conn = url.openConnection();
 
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(conn.getInputStream());
+            
+            NodeList ndl = doc.getElementsByTagName("item");
+            
+            for(int i=0; i < ndl.getLength(); i++ ){
+                NodeList nodl = (NodeList) ndl.item(i);
+                for(int j=0; j < nodl.getLength(); j++ ){
+                    Node nditem = nodl.item(j);
+                    if (nditem.getNodeName().equals("title") ){
+                        setTitle(nditem.getTextContent());
+                    }
+                    if(title.equals("DOLAR")){
+                        if (nditem.getNodeName().equals("description") ){
+                            setDescription(nditem.getTextContent());
+                        }
+                        if (nditem.getNodeName().equals("valueDate") ){
+                            setValueDate(nditem.getTextContent());
+                        }
+                    }
+                }
+            }
+           
+            //System.out.println("title:"+title+" description:"+description+" valueDate:"+valueDate);
+            /*
+            TransformerFactory factory1 = TransformerFactory.newInstance();
+            Transformer xform = factory1.newTransformer();
+            
+            // that’s the default xform; use a stylesheet to get a real one
+            xform.transform(new DOMSource(doc), new StreamResult(System.out));
+            */
+            
+        }
+        catch (ParserConfigurationException ex) {
+            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+        }        catch (SAXException ex) {
+            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+        }        catch (IOException ex) {
+            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
-    
-    
-    
+    public String jobTiposMoneda(String data_string, String extra_data_array){
+        
+        String actualizo = "0";
+        actualizo = this.getGralDao().selectFunctionForThisApp(data_string, extra_data_array);
+        //actualizo = this.getGralDao().selectFunctionForThisApp(data_string, extra_data_array);
+        
+        return actualizo;
+    }
     
     
 
