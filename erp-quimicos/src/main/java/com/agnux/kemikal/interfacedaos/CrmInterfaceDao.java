3119e50df4742b4812947a194fc1f05d14a22ee5
fmora <fmora@ce483f98-b20a-4c78-8f99-c15376fc4320>
2013-03-12 18:16:43 +0000

    git-svn-id: http://ironman.agnux.com/repositorios/ErpAgnuxV1@599 ce483f98-b20a-4c78-8f99-c15376fc4320

--- erp-quimicos/src/main/java/com/agnux/kemikal/interfacedaos/CrmInterfaceDao.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/interfacedaos/CrmInterfaceDao.java
@@ -9,28 +9,28 @@ import java.util.HashMap;
  * @author luis Carrillo
  */
 public interface CrmInterfaceDao {
-    
+
     //metodos  de uso general
     public int countAll(String data_string);
     public HashMap<String, String> selectFunctionValidateAaplicativo(String data, Integer idApp, String extra_data_array);
     public String selectFunctionForThisApp(String campos_data, String extra_data_array);
     public String selectFunctionForCrmAdmProcesos(String campos_data, String extra_data_array);
-    
+
     /*Buscador de contactos*/
     public ArrayList<HashMap<String, String>> getBuscadorContactos(String nombre, String apellidop, String apellidom, String tipo_contacto, Integer id_empresa);
-    
+
     //metodos para aplicativo de Motivos de Visita
     public ArrayList<HashMap<String, String>> getMotivoVisita_Datos(Integer id);
     public ArrayList<HashMap<String, Object>> getMotivosVisita_PaginaGrid(String data_string, int offset, int pageSize, String orderBy, String asc,Integer id_empresa);
-    
+
     //metodos para aplicativo de Formas de Contacto
     public ArrayList<HashMap<String, Object>> getFormasContacto_PaginaGrid(String data_string, int offset, int pageSize, String orderBy, String asc,Integer id_empresa);
     public ArrayList<HashMap<String, String>> getFormasContacto_Datos(Integer id);
-    
+
     //metodos para aplicativo de Motivos de LLamadas
     public ArrayList<HashMap<String, String>> getMotivosLlamada_Datos(Integer id);
     public ArrayList<HashMap<String, Object>> getMotivosLlamada_PaginaGrid(String data_string, int offset, int pageSize, String orderBy, String asc,Integer id_empresa);
-    
+
    //Metodos para Aplicativo de Registro de Llamadas
     public ArrayList<HashMap<String, String>> getMotivos_Llamadas(Integer id_empresa);
     public ArrayList<HashMap<String, String>> getCalificacion_Llamadas(Integer id_empresa);
@@ -38,8 +38,8 @@ public interface CrmInterfaceDao {
     public ArrayList<HashMap<String,String>>getCrmRegistroLlamadas_Datos(Integer id);
     public ArrayList<HashMap<String,Object>>getRegistroLlamadas_PaginaGrid(String data_string, int offset, int pageSize, String orderBy, String asc);
 
-    
-    
+
+
     //Métodos para ASplicativo de Registlro de Visitas
     public ArrayList<HashMap<String, Object>> getCrmRegistroVisitas_PaginaGrid(String data_string, int offset, int pageSize, String orderBy, String asc);
     public ArrayList<HashMap<String, String>> getCrmRegistroVisitas_Datos(Integer id);
@@ -48,14 +48,14 @@ public interface CrmInterfaceDao {
     public ArrayList<HashMap<String, String>> getCrmRegistroVisitas_Motivos(Integer id_empresa);
     public ArrayList<HashMap<String, String>> getCrmRegistroVisitas_Calificaciones(Integer id_empresa);
     public ArrayList<HashMap<String, String>> getCrmRegistroVisitas_Seguimientos(Integer id_empresa);
-    
-    
+
+
     //metodos para aplicativo de Oportunidades
     public ArrayList<HashMap<String, Object>> getOportunidades_PaginaGrid(String data_string, int offset, int pageSize, String orderBy, String asc,Integer id_empresa);
     public ArrayList<HashMap<String, String>> getOportunidad_Datos(Integer id);
     public ArrayList<HashMap<String, String>> getTiposOportunidad();
     public ArrayList<HashMap<String, String>> getEtapasVenta();
-    
+
     //metodos para Catalogo de Registro de prospectos
     public ArrayList<HashMap<String, Object>> getPaises();
     public ArrayList<HashMap<String, Object>> getProspecto_Datos(Integer id);
@@ -67,21 +67,21 @@ public interface CrmInterfaceDao {
     //public ArrayList<HashMap<String, Object>> getEtapas_venta();
     public ArrayList<HashMap<String, Object>> getClasificacion_prospecto();
     public ArrayList<HashMap<String, Object>> getTipo_industria();
-    
+
     public ArrayList<HashMap<String, Object>> getEtapas_prospecto();
-    
-    
-    
+
+
+
     //metodos para el aplicativo de metas
     public ArrayList<HashMap<String, String>> getCrmRegistoMetas_Datos(Integer id);
 
     public ArrayList<HashMap<String, Object>> getRegistroMetas_PaginaGrid(String data_string, int offset, int pageSize, String orderBy, String asc);
-    
+
     //Métodos para ASplicativo de Registlro de Casos
     public ArrayList<HashMap<String, Object>> getCrmRegistroCasos_PaginaGrid(String data_string, int offset, int pageSize, String orderBy, String asc);
     public ArrayList<HashMap<String, String>> getCrmRegistroCasos_Datos(Integer id);
     public ArrayList<HashMap<String, String>> getBuscadorCliente_Prospecto(String Razon_social, String rfc, Integer identificador_cliente_prospecto, Integer id_empresa);
-    
+
     //Metodos para el buscador de Llamadas
 
     public ArrayList<HashMap<String, String>> getBuscadorRegistros(Integer id, Integer agente, Integer tipo_seleccion, Integer status, Integer etapa, String fecha_inicial, String fecha_final, Integer id_empresa);
@@ -94,9 +94,14 @@ public interface CrmInterfaceDao {
     public ArrayList<HashMap<String, String>> getBuscadorCasos(Integer id, Integer agente, Integer tipo_seleccion, Integer status, Integer etapa, String fecha_inicial, String fecha_final, Integer id_empresa);
 
     public ArrayList<HashMap<String, String>> getBuscadorOportunidades(Integer id, Integer agente, Integer tipo_seleccion, Integer status, Integer etapa, String fecha_inicial, String fecha_final, Integer id_empresa);
-   
-    
+
+
     public ArrayList<HashMap<String, Object>> getContactos_PaginaGrid(String data_string, int offset, int pageSize, String orderBy, String asc, Integer id_empresa);
     public ArrayList<HashMap<String, String>> getContacto_Datos(Integer id);
-    
+
+    //CRM  Reporte
+    //CRM  Reporte de visitas
+    public ArrayList<HashMap<String, String>> getVisitas(String fecha_inicial, String fecha_final,Integer id_empresa);
+    //fin de reportes de CRM
+
 }
