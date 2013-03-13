2b0076ff2ea06e83adb1e3f57ca421b968c3a52b
fmora <fmora@ce483f98-b20a-4c78-8f99-c15376fc4320>
2013-03-13 18:58:27 +0000

    git-svn-id: http://ironman.agnux.com/repositorios/ErpAgnuxV1@616 ce483f98-b20a-4c78-8f99-c15376fc4320

---
+++ erp-quimicos/src/main/java/com/agnux/kemikal/controllers/RepInvMovimientosController.java
@@ -0,0 +1,157 @@
+/*
+ * Reporte de Movimientos de (KARDEX)
+ */
+package com.agnux.kemikal.controllers;
+import com.agnux.cfd.v2.Base64Coder;
+import com.agnux.common.helpers.FileHelper;
+import com.agnux.common.helpers.TimeHelper;
+import com.agnux.common.obj.ResourceProject;
+import com.agnux.common.obj.UserSessionData;
+import com.agnux.kemikal.interfacedaos.GralInterfaceDao;
+import com.agnux.kemikal.interfacedaos.HomeInterfaceDao;
+import com.agnux.kemikal.interfacedaos.InvInterfaceDao;
+import com.agnux.kemikal.reportes.PdfReporteInvExisLotes;
+import com.agnux.xml.labels.EtiquetaCompras;
+import com.agnux.xml.labels.generandoxml;
+import com.itextpdf.text.DocumentException;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.TransformerException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+/**
+ *
+ * @author Noe Martinez
+ * gpmarsan@gmail.com
+ * 12/marzo/2013
+ * 
+ */
+@Controller
+@SessionAttributes({"user"})
+@RequestMapping("/repinvmovimientos/")
+public class RepInvMovimientosController {
+    private static final Logger log  = Logger.getLogger(RepInvExisController.class.getName());
+    ResourceProject resource = new ResourceProject();
+    
+    @Autowired
+    @Qualifier("daoInv")
+    private InvInterfaceDao invDao;
+    
+    
+    @Autowired
+    @Qualifier("daoHome")
+    private HomeInterfaceDao HomeDao;
+    
+    @Autowired
+    @Qualifier("daoGral")
+    private GralInterfaceDao gralDao;
+    
+    public InvInterfaceDao getInvDao() {
+        return invDao;
+    }
+    
+    
+    public HomeInterfaceDao getHomeDao() {
+        return HomeDao;
+    }
+    
+    
+    public GralInterfaceDao getGralDao() {
+        return gralDao;
+    }
+    
+    
+    
+    @RequestMapping(value="/startup.agnux")
+    public ModelAndView startUp(HttpServletRequest request, HttpServletResponse response, 
+            @ModelAttribute("user") UserSessionData user)
+            throws ServletException, IOException {
+        
+        log.log(Level.INFO, "Ejecutando starUp de {0}", RepInvMovimientosController.class.getName());
+        LinkedHashMap<String,String> infoConstruccionTabla = new LinkedHashMap<String,String>();
+        
+        
+        ModelAndView x = new ModelAndView("repinvmovimientos/startup", "title", "Movimientos de Inventario");
+        
+        x = x.addObject("layoutheader", resource.getLayoutheader());
+        x = x.addObject("layoutmenu", resource.getLayoutmenu());
+        x = x.addObject("layoutfooter", resource.getLayoutfooter());
+        x = x.addObject("grid", resource.generaGrid(infoConstruccionTabla));
+        x = x.addObject("url", resource.getUrl(request));
+        x = x.addObject("username", user.getUserName());
+        x = x.addObject("empresa", user.getRazonSocialEmpresa());
+        x = x.addObject("sucursal", user.getSucursal());
+        
+        String userId = String.valueOf(user.getUserId());
+        
+        String codificado = Base64Coder.encodeString(userId);
+        
+        //id de usuario codificado
+        x = x.addObject("iu", codificado);
+        
+        return x;
+    }
+    
+    
+    
+    
+    //obtiene datos para el buscador
+    @RequestMapping(method = RequestMethod.POST, value="/getDatosBusqueda.json")
+    public @ResponseBody HashMap<String,ArrayList<HashMap<String, String>>> getAlmacenesJson(
+            @RequestParam(value="iu", required=true) String id_user,
+            Model model
+        ) {
+        
+        log.log(Level.INFO, "Ejecutando getDatosBusquedaJson de {0}", RepInvMovimientosController.class.getName());
+        HashMap<String,ArrayList<HashMap<String, String>>> jsonretorno = new HashMap<String,ArrayList<HashMap<String, String>>>();
+        ArrayList<HashMap<String, String>> Almacenes = new ArrayList<HashMap<String, String>>();
+        ArrayList<HashMap<String, String>> TiposMovimiento = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> userDat = new HashMap<String, String>();
+        
+        //decodificar id de usuario
+        Integer id_usuario = Integer.parseInt(Base64Coder.decodeString(id_user));
+        userDat = this.getHomeDao().getUserById(id_usuario);
+        
+        Integer id_empresa = Integer.parseInt(userDat.get("empresa_id"));
+        
+        Almacenes = this.getInvDao().getAlmacenes2(id_empresa);
+        TiposMovimiento = this.getInvDao().getAllTiposMovimientoInventario(id_empresa);
+        
+        jsonretorno.put("Almacenes", Almacenes);
+        jsonretorno.put("Tmov", TiposMovimiento);
+        
+        return jsonretorno;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
