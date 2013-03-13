1c001fa8c270a8d26d216928965ddfe57676e217
fmora <fmora@ce483f98-b20a-4c78-8f99-c15376fc4320>
2013-03-13 15:45:17 +0000

    git-svn-id: http://ironman.agnux.com/repositorios/ErpAgnuxV1@612 ce483f98-b20a-4c78-8f99-c15376fc4320

--- erp-quimicos/src/main/java/com/agnux/kemikal/reportes/Pdf_PRO_Produccion_diaria.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/reportes/Pdf_PRO_Produccion_diaria.java
@@ -88,6 +88,9 @@ public class Pdf_PRO_Produccion_diaria {
 
                tabla.addCell(celda);
             }
+
+            if(datos_produccion.size()>0){
+
             String folio_orden=datos_produccion.get(0).get("folio_orden").toString();
             Double  suma_cantidad=0.0;
             Integer tmp= 0;
@@ -351,8 +354,9 @@ public class Pdf_PRO_Produccion_diaria {
                         tabla.addCell(cell);
 
 
-
+          }
             doc.add(tabla);
+
             doc.close();
 	} catch (FileNotFoundException ex) {
             Logger.getLogger(Pdf_CRM_registroVisitas.class.getName()).log(Level.SEVERE, null, ex);
