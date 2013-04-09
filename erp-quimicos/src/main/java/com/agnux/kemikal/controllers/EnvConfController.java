8ddcf96a89ffde676a84cea606237f87b350cc4b
fmora <fmora@ce483f98-b20a-4c78-8f99-c15376fc4320>
2013-04-09 21:19:44 +0000

    git-svn-id: http://ironman.agnux.com/repositorios/ErpAgnuxV1@740 ce483f98-b20a-4c78-8f99-c15376fc4320

--- erp-quimicos/src/main/java/com/agnux/kemikal/controllers/EnvConfController.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/controllers/EnvConfController.java
@@ -40,6 +40,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
 /**
  *
  * @author Noe Martinez
