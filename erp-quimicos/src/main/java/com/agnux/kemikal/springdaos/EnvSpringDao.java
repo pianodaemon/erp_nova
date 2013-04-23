81aaa3b44ce20531551d03369513524ed6e15959
fmora <fmora@ce483f98-b20a-4c78-8f99-c15376fc4320>
2013-04-23 00:08:32 +0000

    git-svn-id: http://ironman.agnux.com/repositorios/ErpAgnuxV1@847 ce483f98-b20a-4c78-8f99-c15376fc4320

--- erp-quimicos/src/main/java/com/agnux/kemikal/springdaos/EnvSpringDao.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/springdaos/EnvSpringDao.java
@@ -567,8 +567,8 @@ public class EnvSpringDao implements EnvInterfaceDao{
                 + "JOIN env_conf ON env_conf.id=env_reenv_det.env_conf_id "
                 + "WHERE env_reenv_det.env_reenv_id=?;";
         
-        //System.out.println("sql_to_query: "+sql_to_query);
-        //System.out.println("id: "+id);
+        System.out.println("getDatosGrid: "+sql_to_query);
+        System.out.println("id: "+id);
         ArrayList<HashMap<String, String>> hm = (ArrayList<HashMap<String, String>>) this.jdbcTemplate.query(
             sql_to_query,
             new Object[]{new Integer(id)}, new RowMapper(){
@@ -623,6 +623,7 @@ public class EnvSpringDao implements EnvInterfaceDao{
     public ArrayList<HashMap<String, String>> getReEenv_Existencias(Integer id_prod, Integer id_pres, Integer id_alm) {
         String sql_query = "SELECT exis, decimales FROM (SELECT (inv_exi_pres.cantidad::double precision - inv_exi_pres.reservado::double precision) AS exis, inv_prod_unidades.decimales FROM inv_exi_pres JOIN inv_prod ON inv_prod.id=inv_exi_pres.inv_prod_id JOIN inv_prod_unidades ON inv_prod_unidades.id=inv_prod.unidad_id WHERE inv_exi_pres.inv_alm_id=? AND inv_exi_pres.inv_prod_id=? AND inv_exi_pres.inv_prod_presentacion_id=?)AS sbt WHERE exis>0;";
         System.out.println("getExis: "+sql_query);
+        System.out.println("id_prod:"+id_prod+"    id_pres:"+id_pres+"     id_alm:"+id_alm);
         ArrayList<HashMap<String, String>> hm = (ArrayList<HashMap<String, String>>) this.jdbcTemplate.query(
             sql_query,
             new Object[]{new Integer(id_alm), new Integer(id_prod), new Integer(id_pres)}, new RowMapper() {
