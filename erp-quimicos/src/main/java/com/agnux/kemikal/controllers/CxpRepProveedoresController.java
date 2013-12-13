a86396c835b0dd138f61cabebe7e5e25984f0cdb
gpmarsan <gpmarsan@ce483f98-b20a-4c78-8f99-c15376fc4320>
2013-12-13 22:02:34 +0000

    git-svn-id: http://ironman.agnux.com/repositorios/ErpAgnuxV1@1138 ce483f98-b20a-4c78-8f99-c15376fc4320

---
+++ erp-quimicos/src/main/java/com/agnux/kemikal/controllers/CxpRepProveedoresController.java
@@ -0,0 +1,291 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.agnux.kemikal.controllers;
+
+import com.agnux.cfd.v2.Base64Coder;
+import com.agnux.common.helpers.FileHelper;
+import com.agnux.common.helpers.TimeHelper;
+import com.agnux.common.obj.ResourceProject;
+import com.agnux.common.obj.UserSessionData;
+import com.agnux.kemikal.interfacedaos.CxpInterfaceDao;
+import com.agnux.kemikal.interfacedaos.GralInterfaceDao;
+import com.agnux.kemikal.interfacedaos.HomeInterfaceDao;
+import com.agnux.kemikal.reportes.PdfEstadoCuentaProveedor;
+import com.agnux.kemikal.reportes.PdfReporteAnticiposNoAplicados;
+import com.agnux.kemikal.reportes.PdfReporteClientes;
+import com.agnux.kemikal.reportes.PdfReporteProveedores;
+import com.itextpdf.text.DocumentException;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+/**
+ *
+ * @author agnux
+ */
+@Controller
+@SessionAttributes({"user"})
+@RequestMapping("/repprov/")
+public class CxpRepProveedoresController {
+    
+    private static final Logger log  = Logger.getLogger(CxpRepProveedoresController.class.getName());
+    ResourceProject resource = new ResourceProject();
+    
+    @Autowired
+    @Qualifier("daoCxp")
+    private CxpInterfaceDao cxpDao;
+  
+    
+    @Autowired
+    @Qualifier("daoHome")
+    private HomeInterfaceDao HomeDao;
+    
+    @Autowired
+    @Qualifier("daoGral")
+    private GralInterfaceDao gralDao;
+    
+      
+    public HomeInterfaceDao getHomeDao() {
+        return HomeDao;
+    }
+    
+    public GralInterfaceDao getGralDao() {
+        return gralDao;     
+  /*//obtiene datos para el buscador
+    @RequestMapping(method = RequestMethod.POST, value="/getBuscaProveedores.json")
+    //obtiene los tipos de agente
+    public @ResponseBody HashMap<String,ArrayList<HashMap<String, String>>> getBuscaProveedoresJson(
+           // @RequestParam(value="rfc", required=true) String rfc,
+            //@RequestParam(value="email", required=true) String email,
+           //@RequestParam(value="nombre", required=true) String nombre,
+            @RequestParam(value="iu", required=true) String id_user,Model model
+            ){
+
+        HashMap<String,ArrayList<HashMap<String, String>>> jsonretorno = new HashMap<String,ArrayList<HashMap<String, String>>>();
+        HashMap<String, String> userDat = new HashMap<String, String>();
+
+        //decodificar id de usuario
+        Integer id_usuario = Integer.parseInt(Base64Coder.decodeString(id_user));
+        userDat = this.getHomeDao().getUserById(id_usuario);
+        Integer id_empresa = Integer.parseInt(userDat.get("empresa_id"));
+
+        jsonretorno.put("Proveedores", this.getCxcDao().getAgentes(id_empresa));
+
+        return jsonretorno;
+
+    }*/
+    
+    }
+    
+    public CxpInterfaceDao getCxpDao() {
+        return cxpDao;
+    }
+    
+    
+    @RequestMapping(value="/startup.agnux")
+    public ModelAndView startUp(HttpServletRequest request, HttpServletResponse response, 
+            @ModelAttribute("user") UserSessionData user)
+            throws ServletException, IOException {
+        
+        log.log(Level.INFO, "Ejecutando starUp de {0}", CxpRepProveedoresController.class.getName());
+        LinkedHashMap<String,String> infoConstruccionTabla = new LinkedHashMap<String,String>();
+        
+        ModelAndView x = new ModelAndView("repprov/startup", "title", "Reporte de Proveedores");
+        
+        x = x.addObject("layoutheader", resource.getLayoutheader());
+        x = x.addObject("layoutmenu", resource.getLayoutmenu());
+        x = x.addObject("layoutfooter", resource.getLayoutfooter());
+        x = x.addObject("grid", resource.generaGrid(infoConstruccionTabla));
+        x = x.addObject("url", resource.getUrl(request));
+        x = x.addObject("username", user.getUserName());
+        x = x.addObject("empresa", user.getRazonSocialEmpresa());
+        x = x.addObject("sucursal", user.getSucursal());
+        
+        String userId = String.valueOf(user.getUserId());
+        
+        String codificado = Base64Coder.encodeString(userId);
+        
+        //id de usuario codificado
+        x = x.addObject("iu", codificado);
+        
+        return x;
+    }
+    
+
+    
+    
+          //obtiene los proveedores para el buscador
+    @RequestMapping(method = RequestMethod.POST, value="/getBuscaProveedores.json")
+    public @ResponseBody HashMap<String,ArrayList<HashMap<String, String>>> getBuscaProveedoresJson(
+            @RequestParam(value="rfc", required=true) String rfc,
+            @RequestParam(value="email", required=true) String email,
+            @RequestParam(value="nombre", required=true) String nombre,
+            @RequestParam(value="iu", required=true) String id_user,
+            Model model
+            ) {
+        
+        log.log(Level.INFO, "Ejecutando getBuscaProveedoresJson de {0}", CxpRepProveedoresController.class.getName());
+        HashMap<String,ArrayList<HashMap<String, String>>> jsonretorno = new HashMap<String,ArrayList<HashMap<String, String>>>();
+        ArrayList<HashMap<String, String>> proveedores = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> userDat = new HashMap<String, String>();
+        //decodificar id de usuario
+        Integer id_usuario = Integer.parseInt(Base64Coder.decodeString(id_user));
+        userDat = this.getHomeDao().getUserById(id_usuario);
+        
+        Integer id_empresa = Integer.parseInt(userDat.get("empresa_id"));
+        
+        proveedores = this.getCxpDao().getBuscadorProveedores(rfc, email, nombre,id_empresa);
+        
+        jsonretorno.put("Proveedores", proveedores);
+        
+        return jsonretorno;
+    }
+
+
+    
+    @RequestMapping(method = RequestMethod.POST, value = "/getReporteProveedores.json")
+    public @ResponseBody
+    HashMap<String, ArrayList<HashMap<String, String>>> getReporteProveedoresJson(
+            //@RequestParam(value = "id_agente", required = true) Integer id_agente,
+            @RequestParam("folio") String folio,
+           @RequestParam("proveedor") String razon_proveedor,
+          
+            @RequestParam(value = "iu", required = true) String id_user,
+            Model model) {
+
+        log.log(Level.INFO, "Ejecutando getMovimientosJson de {0}", PdfReporteProveedores.class.getName());
+        HashMap<String, ArrayList<HashMap<String, String>>> jsonretorno = new HashMap<String, ArrayList<HashMap<String, String>>>();
+        ArrayList<HashMap<String, String>> proveedores = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> userDat = new HashMap<String, String>();
+        //decodificar id de usuario
+        Integer id_usuario = Integer.parseInt(Base64Coder.decodeString(id_user));
+        userDat = this.getHomeDao().getUserById(id_usuario);
+
+        Integer id_empresa = Integer.parseInt(userDat.get("empresa_id"));
+        //proveedores = this.getCxpDao().getListaProveedores(proveedor,id_empresa);
+        proveedores = this.getCxpDao().getListaProveedores(folio, razon_proveedor, id_empresa);
+        
+
+        jsonretorno.put("Proveedores", proveedores);
+
+        return jsonretorno;
+    }
+    
+    
+    
+     //Genera pdf Reporte de Movimientos
+    @RequestMapping(value = "/getReporteProveedores/{cadena}/{iu}/out.json", method = RequestMethod.GET )
+    public ModelAndView getGeneraPdfRemisionJson(
+                @PathVariable("cadena") String cadena,
+                @PathVariable("iu") String id_user,
+                HttpServletRequest request,
+                HttpServletResponse response,
+                Model model)
+        throws ServletException, IOException, URISyntaxException, DocumentException {
+
+        HashMap<String, String> userDat = new HashMap<String, String>();
+        HashMap<String, String> datosEncabezadoPie= new HashMap<String, String>();
+        ArrayList<HashMap<String, String>> proveedores = new ArrayList<HashMap<String, String>>();
+
+        Integer app_selected = 156; //Aqui se deja el aplicativo 156 Reporte de Proveedores.
+        
+        System.out.println("Generando Reporte de Proveedores");
+       String folio = "";
+       String razon_proveedor="";
+
+
+        String arrayCad [] = cadena.split("___");
+
+        if (!arrayCad[0].equals("0")){
+            folio = arrayCad[0];
+        }
+        
+        if (!arrayCad[1].equals("0")){
+            razon_proveedor = arrayCad[1];
+        }
+
+      
+        //decodificar id de usuario
+        Integer id_usuario = Integer.parseInt(Base64Coder.decodeString(id_user));
+        userDat = this.getHomeDao().getUserById(id_usuario);
+        Integer id_empresa = Integer.parseInt(userDat.get("empresa_id"));
+        String rfc=this.getGralDao().getRfcEmpresaEmisora(id_empresa);
+
+        String razon_social_empresa = this.getGralDao().getRazonSocialEmpresaEmisora(id_empresa);
+
+        //fecha de impresion..
+        String nombreMes= TimeHelper.ConvertNumToMonth(Integer.parseInt(TimeHelper.getMesActual()));
+
+        SimpleDateFormat formato = new SimpleDateFormat("'Impreso el' d 'de "+nombreMes+" del ' yyyy 'a las' HH:mm:ss 'hrs.'");
+        String impreso_en = formato.format(new Date());
+
+        //obtener el directorio temporal
+        String dir_tmp = this.getGralDao().getTmpDir();
+        
+        datosEncabezadoPie.put("empresa", razon_social_empresa);
+        datosEncabezadoPie.put("titulo_reporte", this.getGralDao().getTituloReporte(id_empresa, app_selected));
+        datosEncabezadoPie.put("periodo", impreso_en);
+        datosEncabezadoPie.put("codigo1", this.getGralDao().getCodigo1Iso(id_empresa, app_selected));
+        datosEncabezadoPie.put("codigo2", this.getGralDao().getCodigo2Iso(id_empresa, app_selected));
+        
+
+
+        File file_dir_tmp = new File(dir_tmp);
+        System.out.println("Directorio temporal: "+file_dir_tmp.getCanonicalPath());
+
+        String file_name = "REPPROVEEDORES_"+nombreMes+".pdf";
+
+        //ruta de archivo de salida
+        String fileout = file_dir_tmp +"/"+  file_name;
+        
+        
+        proveedores = this.getCxpDao().getListaProveedores(folio, razon_proveedor, id_empresa);
+          
+        PdfReporteProveedores x = new PdfReporteProveedores(datosEncabezadoPie,fileout,proveedores);
+
+        System.out.println("Recuperando archivo: " + fileout);
+        File file = new File(fileout);
+        int size = (int) file.length(); // Tamaño del archivo
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+        response.setBufferSize(size);
+        response.setContentLength(size);
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition","attachment; filename=\"" + file.getCanonicalPath() +"\"");
+        FileCopyUtils.copy(bis, response.getOutputStream());
+        response.flushBuffer();
+
+        return null;
+    }
+
+    
+    
+    
+    
+}
