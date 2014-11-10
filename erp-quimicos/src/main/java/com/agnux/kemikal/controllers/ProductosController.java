bd2770132e63b49e1b226421ec2249da67341b91
gpmarsan <gpmarsan@ce483f98-b20a-4c78-8f99-c15376fc4320>
2014-11-10 15:48:02 +0000

    git-svn-id: http://ironman.agnux.com/repositorios/ErpAgnuxV1@1494 ce483f98-b20a-4c78-8f99-c15376fc4320

--- erp-quimicos/src/main/java/com/agnux/kemikal/controllers/ProductosController.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/controllers/ProductosController.java
@@ -25,19 +25,13 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 
 
