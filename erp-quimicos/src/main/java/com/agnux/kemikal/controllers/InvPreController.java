8b636991692d409338b003e98092a9db5bc60fd0
gpmarsan <gpmarsan@ce483f98-b20a-4c78-8f99-c15376fc4320>
2014-11-12 04:10:36 +0000

    git-svn-id: http://ironman.agnux.com/repositorios/ErpAgnuxV1@1499 ce483f98-b20a-4c78-8f99-c15376fc4320

--- erp-quimicos/src/main/java/com/agnux/kemikal/controllers/InvPreController.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/controllers/InvPreController.java
@@ -4,14 +4,13 @@
  */
 package com.agnux.kemikal.controllers;
 
-import com.agnux.kemikal.interfacedaos.InvInterfaceDao;
-
 import com.agnux.cfd.v2.Base64Coder;
 import com.agnux.common.helpers.StringHelper;
 import com.agnux.common.obj.DataPost;
 import com.agnux.common.obj.ResourceProject;
 import com.agnux.common.obj.UserSessionData;
 import com.agnux.kemikal.interfacedaos.HomeInterfaceDao;
+import com.agnux.kemikal.interfacedaos.InvInterfaceDao;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,17 +20,12 @@ import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Paco Mora
--- erp-quimicos/src/main/java/com/agnux/kemikal/controllers/LogServAdicController.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/controllers/LogServAdicController.java
@@ -8,7 +8,6 @@ package com.agnux.kemikal.controllers;
 
 import com.agnux.cfd.v2.Base64Coder;
 import com.agnux.common.helpers.StringHelper;
-import com.agnux.common.helpers.TimeHelper;
 import com.agnux.common.obj.DataPost;
 import com.agnux.common.obj.ResourceProject;
 import com.agnux.common.obj.UserSessionData;
