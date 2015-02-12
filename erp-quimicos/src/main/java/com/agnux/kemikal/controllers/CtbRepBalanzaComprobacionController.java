be024fb2ae08cb11691c62605c1957d69a6859f7
gpmarsan <gpmarsan@ce483f98-b20a-4c78-8f99-c15376fc4320>
2015-02-12 04:27:40 +0000

    git-svn-id: http://ironman.agnux.com/repositorios/ErpAgnuxV1@1582 ce483f98-b20a-4c78-8f99-c15376fc4320

--- erp-quimicos/src/main/java/com/agnux/kemikal/controllers/CtbRepBalanzaComprobacionController.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/controllers/CtbRepBalanzaComprobacionController.java
@@ -7,7 +7,6 @@ package com.agnux.kemikal.controllers;
 import com.agnux.cfd.v2.Base64Coder;
 import com.agnux.common.helpers.StringHelper;
 import com.agnux.common.helpers.TimeHelper;
-import com.agnux.common.obj.DataPost;
 import com.agnux.common.obj.ResourceProject;
 import com.agnux.common.obj.UserSessionData;
 import com.agnux.kemikal.interfacedaos.CtbInterfaceDao;
@@ -30,19 +29,13 @@ import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
