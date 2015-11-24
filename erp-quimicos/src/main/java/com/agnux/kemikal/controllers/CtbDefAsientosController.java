3773c7d5be1041d6d53fd04e646967fe05dd09cc
gpmarsan <gpmarsan@ce483f98-b20a-4c78-8f99-c15376fc4320>
2015-11-24 19:14:44 +0000

    git-svn-id: http://ironman.agnux.com/repositorios/ErpAgnuxV1@1732 ce483f98-b20a-4c78-8f99-c15376fc4320

---
+++ erp-quimicos/src/main/java/com/agnux/kemikal/controllers/CtbDefAsientosController.java
@@ -0,0 +1,101 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.agnux.kemikal.controllers;
+
+import com.agnux.cfd.v2.Base64Coder;
+import com.agnux.common.obj.ResourceProject;
+import com.agnux.common.obj.UserSessionData;
+import com.agnux.kemikal.interfacedaos.CtbInterfaceDao;
+import com.agnux.kemikal.interfacedaos.HomeInterfaceDao;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Noe Martinez
+ * gpmarsan@gmail.com
+ * 24/noviembre/2015
+ * 
+ */
+@Controller
+@SessionAttributes({"user"})
+@RequestMapping("/ctbdefasientos/")
+public class CtbDefAsientosController {
+    ResourceProject resource = new ResourceProject();
+    private static final Logger log  = Logger.getLogger(CtbDefAsientosController.class.getName());
+    
+    @Autowired
+    @Qualifier("daoCtb")
+    private CtbInterfaceDao ctbDao;
+    
+    public CtbInterfaceDao getCtbDao() {
+        return ctbDao;
+    }
+    
+    @Autowired
+    @Qualifier("daoHome")
+    private HomeInterfaceDao HomeDao;
+    
+    public HomeInterfaceDao getHomeDao() {
+        return HomeDao;
+    }
+    
+    @RequestMapping(value="/startup.agnux")
+    public ModelAndView startUp(HttpServletRequest request, HttpServletResponse response, 
+            @ModelAttribute("user") UserSessionData user)
+            throws ServletException, IOException {
+        log.log(Level.INFO, "Ejecutando starUp de {0}", CtbPolizasContablesController.class.getName());
+        LinkedHashMap<String,String> infoConstruccionTabla = new LinkedHashMap<String,String>();
+        
+        infoConstruccionTabla.put("id", "Acciones:90");
+        infoConstruccionTabla.put("poliza", "P&oacute;liza:80");
+        infoConstruccionTabla.put("tipo", "Tipo de P&oacute;liza:170");
+        infoConstruccionTabla.put("concepto","Concepto:170");
+        infoConstruccionTabla.put("fecha","Fecha:80");
+        infoConstruccionTabla.put("moneda", "Moneda:60");
+        infoConstruccionTabla.put("debe","Debe:90");
+        infoConstruccionTabla.put("haber","Haber:90");
+        infoConstruccionTabla.put("status","Estatus:90");
+        
+                    
+        ModelAndView x = new ModelAndView("ctbdefasientos/startup", "title", "Definici&oacute;n de Asientos");
+        
+        x = x.addObject("layoutheader", resource.getLayoutheader());
+        x = x.addObject("layoutmenu", resource.getLayoutmenu());
+        x = x.addObject("layoutfooter", resource.getLayoutfooter());
+        x = x.addObject("grid", resource.generaGrid(infoConstruccionTabla));
+        x = x.addObject("url", resource.getUrl(request));
+        x = x.addObject("username", user.getUserName());
+        x = x.addObject("empresa", user.getRazonSocialEmpresa());
+        x = x.addObject("sucursal", user.getSucursal());
+        
+        String userId = String.valueOf(user.getUserId());
+        
+        String codificado = Base64Coder.encodeString(userId);
+        
+        //id de usuario codificado
+        x = x.addObject("iu", codificado);
+        
+        return x;
+    }
+    
+    
+    
+    
+    
+    
+}
