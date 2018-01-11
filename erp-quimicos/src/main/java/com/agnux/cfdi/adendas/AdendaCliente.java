eaea165d3549261d9188c9a96461b88dc34ad92b
Bob Ross <pianodaemon@gmail.com>
2018-01-11 15:16:15 -0600

    r1: removed needless adendas for cfdi 2
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

--- erp-quimicos/src/main/java/com/agnux/cfdi/adendas/AdendaCliente.java
+++ erp-quimicos/src/main/java/com/agnux/cfdi/adendas/AdendaCliente.java
@@ -17,14 +17,14 @@ public class AdendaCliente extends Adenda{
     @Override
     public void createAdenda(Integer noAdenda, LinkedHashMap<String, Object> dataAdenda, String dirXml, String fileNameXml) {
         AdendaFemsaQuimiproductos femsaQuimiproductos = new AdendaFemsaQuimiproductos();
-        AdendaSunchemical sunChemical = new AdendaSunchemical();
-        AdendaComex comex = new AdendaComex();
+        //AdendaSunchemical sunChemical = new AdendaSunchemical();
+        //AdendaComex comex = new AdendaComex();
         //Adenda soriana = new AdendaSoriana();
         //Adenda walmart = new AdendaWalmart();
         
         this.SetNext(femsaQuimiproductos);
-        femsaQuimiproductos.SetNext(sunChemical);
-        sunChemical.SetNext(comex);
+       // femsaQuimiproductos.SetNext(sunChemical);
+        //sunChemical.SetNext(comex);
         //lala.SetNext(soriana);
         //soriana.SetNext(walmart);
         
