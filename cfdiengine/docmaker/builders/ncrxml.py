30d6ff76b89ba783f4321ce935aedb781731485b
Bob Ross <pianodaemon@gmail.com>
2018-01-15 23:55:24 -0600

    docmaker: fixed typos inside ncrxml
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

--- cfdiengine/docmaker/builders/ncrxml.py
+++ cfdiengine/docmaker/builders/ncrxml.py
@@ -9,7 +9,7 @@ from decimal import Decimal
 from misc.helperstr import HelperStr
 from docmaker.error import DocBuilderStepError
 from misc.tricks import truncate
-from docmaker.en import BuilderGen
+from docmaker.gen import BuilderGen
 from sat.v33 import Comprobante
 from sat.requirement import writedom_cfdi, sign_cfdi
 from sat.artifacts import CfdiType
@@ -75,9 +75,9 @@ class NcrXml(BuilderGen):
             JOIN gral_suc AS SUC on NC.gral_suc_id = SUC.id
             JOIN gral_imptos AS GI ON GI.id = SUC.gral_impto_id
             WHERE NC.id = """
+        rowset = []
         for row in self.pg_query(conn, "{0}{1}".format(q, nc_id)):
-            # Just taking first row of query result
-            return {
+            rowset.append({
                 'PRODSERV': row['clave_prod'],
                 'UNIDAD': row['clave_unidad'],
                 'CANTIDAD': row['cantidad'],
@@ -91,7 +91,8 @@ class NcrXml(BuilderGen):
                 'TASA_IMPUESTO': row['tasa_impuesto'],
                 'IEPS_ID': row['ieps_id'],
                 'IMPUESTO_ID': row['impto_id']
-            }
+            })
+        return rowset
 
     def __q_no_certificado(self, conn, usr_id):
         """
@@ -99,9 +100,9 @@ class NcrXml(BuilderGen):
         """
         q = """select CFDI_CONF.numero_certificado
             FROM gral_suc AS SUC
-            LEFT JOIN gral_usr_suc AS USR_SUC ON USR_SUC.ral_suc_id = SUC.id
-            LEFT JOIN fac_cfds_conf AS CFDI_CONF ON CFDI_CONF.ral_suc_id = SUC.id
-            WHERE USR_SUC.ral_usr_id = """
+            LEFT JOIN gral_usr_suc AS USR_SUC ON USR_SUC.gral_suc_id = SUC.id
+            LEFT JOIN fac_cfds_conf AS CFDI_CONF ON CFDI_CONF.gral_suc_id = SUC.id
+            WHERE USR_SUC.gral_usr_id = """
         for row in self.pg_query(conn, "{0}{1}".format(q, usr_id)):
             # Just taking first row of query result
             return row['numero_certificado']
@@ -113,14 +114,20 @@ class NcrXml(BuilderGen):
         q = """select fac_cfds_conf_folios.serie as serie,
             fac_cfds_conf_folios.folio_actual::character varying as folio
             FROM gral_suc AS SUC
-            LEFT JOIN fac_cfds_conf ON fac_cfds_conf.ral_suc_id = SUC.id
+            LEFT JOIN fac_cfds_conf ON fac_cfds_conf.gral_suc_id = SUC.id
             LEFT JOIN fac_cfds_conf_folios ON fac_cfds_conf_folios.fac_cfds_conf_id = fac_cfds_conf.id
-            LEFT JOIN gral_usr_suc AS USR_SUC ON USR_SUC.ral_suc_id = SUC.id
+            LEFT JOIN gral_usr_suc AS USR_SUC ON USR_SUC.gral_suc_id = SUC.id
             WHERE fac_cfds_conf_folios.proposito = 'NCR'
-            AND USR_SUC.ral_usr_id = """
+            AND USR_SUC.gral_usr_id = """
         for row in self.pg_query(conn, "{0}{1}".format(q, usr_id)):
             # Just taking first row of query result
             return { 'SERIE': row['serie'], 'FOLIO': row['folio'] }
+
+    def __q_forma_pago(self, conn, prefact_id):
+        """
+        Consulta la forma de pago y numero de cuenta
+        """
+        return { 'CLAVE': '03', 'CUENTA': None }
 
     def __q_moneda(self, conn, nc_id):
         """
@@ -146,7 +153,7 @@ class NcrXml(BuilderGen):
         Consulta el cliente de la nc en dbms
         """
         q = """select cxc_clie.rfc as rfc,
-            cxc_clie.razon_social as razon_social,
+            cxc_clie.razon_social as razon_social
             FROM fac_nota_credito
             LEFT JOIN cxc_clie ON cxc_clie.id = fac_nota_credito.cxc_clie_id
             WHERE fac_nota_credito.id = """
@@ -154,7 +161,8 @@ class NcrXml(BuilderGen):
             # Just taking first row of query result
             return {
                 'RFC': row['rfc'],
-                'RAZON_SOCIAL': unidecode.nidecode(row['razon_social']),
+                'RAZON_SOCIAL': unidecode.unidecode(row['razon_social']),
+                'USO_CFDI': 'G02'
             }
 
     def __q_emisor(self, conn, usr_id):
@@ -164,15 +172,15 @@ class NcrXml(BuilderGen):
         q = """select upper(EMP.rfc) as rfc, upper(EMP.titulo) as titulo,
             upper(REG.numero_control) as numero_control
             FROM gral_suc AS SUC
-            LEFT JOIN gral_usr_suc AS USR_SUC ON USR_SUC.ral_suc_id = SUC.id
+            LEFT JOIN gral_usr_suc AS USR_SUC ON USR_SUC.gral_suc_id = SUC.id
             LEFT JOIN gral_emp AS EMP ON EMP.id = SUC.empresa_id
             LEFT JOIN cfdi_regimenes AS REG ON REG.numero_control = EMP.regimen_fiscal
-            WHERE USR_SUC.ral_usr_id = """
+            WHERE USR_SUC.gral_usr_id = """
         for row in self.pg_query(conn, "{0}{1}".format(q, usr_id)):
             # Just taking first row of query result
             return {
                 'RFC': row['rfc'],
-                'RAZON_SOCIAL': unidecode.nidecode(row['titulo']),
+                'RAZON_SOCIAL': unidecode.unidecode(row['titulo']),
                 'REGIMEN_FISCAL': row['numero_control']
             }
 
@@ -316,11 +324,11 @@ class NcrXml(BuilderGen):
     def __calc_totales(self, l_items):
 
         totales = {
-            'MONTO_TOTAL': Decimal(0),
-            'IMPORTE_SUM': Decimal(0),
-            'IMPORTE_SUM_IMPUESTO': Decimal(0),
-            'IMPORTE_SUM_IEPS': Decimal(0),
-            'DESCTO_SUM': Decimal(0),
+            'MONTO_TOTAL': Decimal('0'),
+            'IMPORTE_SUM': Decimal('0'),
+            'IMPORTE_SUM_IMPUESTO': Decimal('0'),
+            'IMPORTE_SUM_IEPS': Decimal('0'),
+            'DESCTO_SUM': Decimal('0'),
         }
 
         for item in l_items:
@@ -345,7 +353,7 @@ class NcrXml(BuilderGen):
 
     def data_acq(self, conn, d_rdirs, **kwargs):
 
-        usr_id = kwargs.et('usr_id', None)
+        usr_id = kwargs.get('usr_id', None)
 
         if usr_id is None:
             raise DocBuilderStepError("user id not fed")
@@ -363,12 +371,14 @@ class NcrXml(BuilderGen):
             content = f.read()
             certb64 = base64.b64encode(content).decode('ascii')
 
-        nc_id = kwargs.et('nc_id', None)
+        nc_id = kwargs.get('nc_id', None)
 
         if nc_id is None:
             raise DocBuilderStepError("nc id not fed")
 
         conceptos = self.__q_conceptos(conn, nc_id)
+        traslados = self.__calc_traslados(conceptos,
+            self.__q_ieps(conn, usr_id), self.__q_ivas(conn, usr_id))
 
         return {
             'TIME_STAMP': '{0:%Y-%m-%dT%H:%M:%S}'.format(datetime.datetime.now()),
@@ -379,9 +389,12 @@ class NcrXml(BuilderGen):
             'EMISOR': ed,
             'RECEPTOR': self.__q_receptor(conn, nc_id),
             'MONEDA': self.__q_moneda(conn, nc_id),
+            'FORMA_PAGO': self.__q_forma_pago(conn, nc_id),
             'NUMERO_CERTIFICADO': self.__q_no_certificado(conn, usr_id),
             'LUGAR_EXPEDICION': self.__q_lugar_expedicion(conn, usr_id),
             'CONCEPTOS': conceptos,
+            'TRASLADOS': traslados,
+            'TOTALES': self.__calc_totales(conceptos)
         }
 
     def format_wrt(self, output_file, dat):
@@ -422,6 +435,7 @@ class NcrXml(BuilderGen):
         c.Folio = dat['CONTROL']['FOLIO']  # optional
         c.Fecha = dat['TIME_STAMP']
         c.Sello = '__DIGITAL_SIGN_HERE__'
+        c.FormaPago = dat["FORMA_PAGO"]['CLAVE']  # optional
         c.NoCertificado = dat['NUMERO_CERTIFICADO']
         c.Certificado = dat['CERT_B64']
         c.TipoDeComprobante = 'E'
@@ -442,6 +456,7 @@ class NcrXml(BuilderGen):
         c.Receptor = pyxb.BIND()
         c.Receptor.Nombre = dat['RECEPTOR']['RAZON_SOCIAL']  # optional
         c.Receptor.Rfc = dat['RECEPTOR']['RFC']
+        c.Receptor.UsoCFDI = dat['RECEPTOR']['USO_CFDI']
 
         c.Conceptos = pyxb.BIND()
         for i in dat['CONCEPTOS']:
