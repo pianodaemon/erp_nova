df030f688216bf51dc39b02e64fb32e8c1f883d2
Bob Ross <pianodaemon@gmail.com>
2018-02-26 17:44:11 -0600

    r1: fixed typo
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

--- erp-quimicos/src/main/java/com/agnux/kemikal/interfacedaos/PocInterfaceDao.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/interfacedaos/PocInterfaceDao.java
@@ -3,6 +3,8 @@
  * and open the template in the editor.
  */
 package com.agnux.kemikal.interfacedaos;
+
+import com.agnux.kemikal.controllers.PotCatCusorder;
 import java.util.ArrayList;
 import java.util.HashMap;
 /**
@@ -130,4 +132,8 @@ public interface PocInterfaceDao{
     
     //Verifica si la Cotizacion no tiene precios pendientes de utorizar
     public HashMap<String, Object> getVerificarCotizacion(String folio_cotizacion, Integer id_suc);
+
+    public HashMap<String, String> poc_val_cusorder(Integer usr_id, String curr_val, String date_lim, Integer pay_met, String account, String matrix);
+
+    public String poc_cat_cusorder(PotCatCusorder pc);
 }
